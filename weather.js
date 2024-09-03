e0f52a12f1aedad958d9f24ff1de78047f75a423
Bazztube <80954930+baztube@users.noreply.github.com>
2024-09-03 15:01:45 +0200

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,348 @@
+const codeLocations = {
+    "0": {
+        "day": {
+            "description": "Sunny",
+            "image": "./Icons/Sunny.png"
+        },
+        "night": {
+            "description": "Clear",
+            "image": "./Icons/Clear.png"
+        }
+    },
+    "1": {
+        "day": {
+            "description": "Mostly Sunny",
+            "image": "./Icons/Sunny.png"
+        },
+        "night": {
+            "description": "Mostly Clear",
+            "image": "./Icons/Clear.png"
+        }
+    },
+    "2": {
+        "day": {
+            "description": "Partly Cloudy",
+            "image": "./Icons/PartialyCloudy.png"
+        },
+        "night": {
+            "description": "Partly Cloudy",
+            "image": "./Icons/PartialyCloudyNight.png"
+        }
+    },
+    "3": {
+        "day": {
+            "description": "Cloudy",
+            "image": "./Icons/CloudyNight.png"
+        },
+        "night": {
+            "description": "Cloudy",
+            "image": "./Icons/PartialyCloudyNight.png"
+        }
+    },
+    "45": {
+        "day": {
+            "description": "Foggy",
+            "image": "./Icons/Fog.png"
+        },
+        "night": {
+            "description": "Foggy",
+            "image": "./Icons/FogNight.png"
+        }
+    },
+    "48": {
+        "day": {
+            "description": "Foggy",
+            "image": "./Icons/Fog.png"
+        },
+        "night": {
+            "description": "Foggy",
+            "image": "./Icons/FogNight.png"
+        }
+    },
+    "51": {
+        "day": {
+            "description": "Light Drizzle",
+            "image": "./Icons/Drizzle.png"
+        },
+        "night": {
+            "description": "Light Drizzle",
+            "image": "./Icons/Drizzle.png"
+        }
+    },
+    "53": {
+        "day": {
+            "description": "Drizzle",
+            "image": "./Icons/Drizzle.png"
+        },
+        "night": {
+            "description": "Drizzle",
+            "image": "./Icons/Drizzle.png"
+        }
+    },
+    "55": {
+        "day": {
+            "description": "Heavy Drizzle",
+            "image": "./Icons/Drizzle.png"
+        },
+        "night": {
+            "description": "Heavy Drizzle",
+            "image": "./Icons/Drizzle.png"
+        }
+    },
+    "56": {
+        "day": {
+            "description": "Light Freezing Drizzle",
+            "image": "./Icons/FreezingRain.png"
+        },
+        "night": {
+            "description": "Light Freezing Drizzle",
+            "image": "./Icons/FreezingRain.png"
+        }
+    },
+    "57": {
+        "day": {
+            "description": "Freezing Drizzle",
+            "image": "./Icons/FreezingRain.png"
+        },
+        "night": {
+            "description": "Freezing Drizzle",
+            "image": "./Icons/FreezingRain.png"
+        }
+    },
+    "61": {
+        "day": {
+            "description": "Light Rain",
+            "image": "./Icons/Drizzle.png"
+        },
+        "night": {
+            "description": "Light Rain",
+            "image": "./Icons/Drizzle.png"
+        }
+    },
+    "63": {
+        "day": {
+            "description": "Rain",
+            "image": "./Icons/Rain.png"
+        },
+        "night": {
+            "description": "Rain",
+            "image": "./Icons/Rain.png"
+        }
+    },
+    "65": {
+        "day": {
+            "description": "Heavy Rain",
+            "image": "./Icons/HeavyRain.png"
+        },
+        "night": {
+            "description": "Heavy Rain",
+            "image": "./Icons/HeavyRain.png"
+        }
+    },
+    "66": {
+        "day": {
+            "description": "Light Freezing Rain",
+            "image": "./Icons/FreezingRain.png"
+        },
+        "night": {
+            "description": "Light Freezing Rain",
+            "image": "./Icons/FreezingRain.png"
+        }
+    },
+    "67": {
+        "day": {
+            "description": "Freezing Rain",
+            "image": "./Icons/FreezingRain.png"
+        },
+        "night": {
+            "description": "Freezing Rain",
+            "image": "./Icons/FreezingRain.png"
+        }
+    },
+    "71": {
+        "day": {
+            "description": "Light Snow",
+            "image": "./Icons/Snow.png"
+        },
+        "night": {
+            "description": "Light Snow",
+            "image": "./Icons/Snow.png"
+        }
+    },
+    "73": {
+        "day": {
+            "description": "Snow",
+            "image": "./Icons/Snow.png"
+        },
+        "night": {
+            "description": "Snow",
+            "image": "./Icons/Snow.png"
+        }
+    },
+    "75": {
+        "day": {
+            "description": "Heavy Snow",
+            "image": "./Icons/Snow.png"
+        },
+        "night": {
+            "description": "Heavy Snow",
+            "image": "./Icons/Snow.png"
+        }
+    },
+    "77": {
+        "day": {
+            "description": "Snow Grains",
+            "image": "./Icons/Snow.png"
+        },
+        "night": {
+            "description": "Snow Grains",
+            "image": "./Icons/Snow.png"
+        }
+    },
+    "80": {
+        "day": {
+            "description": "Light Showers",
+            "image": "./Icons/Drizzle.png"
+        },
+        "night": {
+            "description": "Light Showers",
+            "image": "./Icons/Drizzle.png"
+        }
+    },
+    "81": {
+        "day": {
+            "description": "Showers",
+            "image": "./Icons/Drizzle.png"
+        },
+        "night": {
+            "description": "Showers",
+            "image": "./Icons/Drizzle.png"
+        }
+    },
+    "82": {
+        "day": {
+            "description": "Heavy Showers",
+            "image": "./Icons/Drizzle.png"
+        },
+        "night": {
+            "description": "Heavy Showers",
+            "image": "./Icons/Drizzle.png"
+        }
+    },
+    "85": {
+        "day": {
+            "description": "Light Snow Showers",
+            "image": "./Icons/Snow.png"
+        },
+        "night": {
+            "description": "Light Snow Showers",
+            "image": "./Icons/Snow.png"
+        }
+    },
+    "86": {
+        "day": {
+            "description": "Snow Showers",
+            "image": "./Icons/Snow.png"
+        },
+        "night": {
+            "description": "Snow Showers",
+            "image": "./Icons/Snow.png"
+        }
+    },
+    "95": {
+        "day": {
+            "description": "Thunderstorm",
+            "image": "./Icons/Thunderstorm.png"
+        },
+        "night": {
+            "description": "Thunderstorm",
+            "image": "./Icons/Thunderstorm.png"
+        }
+    },
+    "96": {
+        "day": {
+            "description": "Light Thunderstorms With Hail",
+            "image": "./Icons/Thunderstorm.png"
+        },
+        "night": {
+            "description": "Light Thunderstorms With Hail",
+            "image": "./Icons/Thunderstorm.png"
+        }
+    },
+    "99": {
+        "day": {
+            "description": "Thunderstorm With Hail",
+            "image": "./Icons/Thunderstorm.png"
+        },
+        "night": {
+            "description": "Thunderstorm With Hail",
+            "image": "./Icons/Thunderstorm.png"
+        }
+    }
+}
+
+let locationName = ""
+let temp = ""
+let minTemp = ""
+let maxTemp = ""
+let codeIcon = ""
+let codeText = ""
+let day = ""
+let units = ""
+
+function getData() {
+    navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude
+        const lon = position.coords.longitude
+        fetch(`https://geocode.maps.co/reverse?lat=${lat}&lon=${lon}&api_key=66d5719bb0cce552213082lnefed200`)
+            .then(response => response.json())
+            .then(data => {
+                locationName = data.address.city
+                document.getElementById("city").innerHTML = locationName
+                console.log(locationName);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        console.log(`https://geocode.maps.co/reverse?lat=${lat}&lon=${lon}&api_key=66d5719bb0cce552213082lnefed200`)
+        console.log(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,is_day,weather_code&minutely_15=temperature_2m&daily=weather_code&timezone=auto&forecast_days=1&forecast_minutely_15=24`)
+        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,is_day,weather_code&minutely_15=temperature_2m&daily=weather_code&timezone=auto&forecast_days=1&forecast_minutely_15=24`)
+            .then(response => response.json())
+            .then(data => {
+                temp = Math.round(data.current.temperature_2m)
+                minTemp = Math.round(Math.min(...data.minutely_15.temperature_2m))
+                maxTemp = Math.round(Math.max(...data.minutely_15.temperature_2m))
+                code = data.current.weather_code - 1
+                day = data.current.is_day
+                units = data.current_units.temperature_2m
+                if (day = 1) {
+                    codeIcon = codeLocations[code].day.image
+                    codeText = codeLocations[code].day.description
+                } else {
+                    codeIcon = codeLocations[code].night.image
+                    codeText = codeLocations[code].night.description
+                }
+                console.log(temp)
+                console.log(minTemp)
+                console.log(maxTemp)
+                console.log(codeIcon)
+                console.log(codeText)
+                console.log(code)
+                console.log(day)
+                console.log(units)
+                document.getElementById("temp").innerHTML = temp + "°"
+                document.getElementById("codeIcon").src = codeIcon
+                document.getElementById("codeText").innerHTML = codeText
+                document.getElementById("minMaxTemp").innerHTML = `H:${maxTemp}° L:${minTemp}°`
+                console.clear
+                console.log('%c Weather data refreshed ', 'color: #0f0')
+
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+
+    });
+}
+setInterval(getData(), 300000)
+
